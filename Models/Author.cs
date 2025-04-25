cd13fbe815757827fa5d54d0e84f1b9602186b41
Khanh Du <luongkhanhdu95@gmail.com>
2025-04-25 10:31:24 +0700

    Update view for user

--- Models/Author.cs
+++ Models/Author.cs
@@ -5,8 +5,12 @@ namespace LibrarySystem.Models
     public class Author
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Name is required")]
+        [RegularExpression(@"\S+.*", ErrorMessage = "Name cannot be just whitespace")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [RegularExpression(@"\S+.*", ErrorMessage = "Description cannot be just whitespace")]
         public string Description { get; set; } = string.Empty;
         public ICollection<Book>? Books { get; set; } = new List<Book>();
     }
