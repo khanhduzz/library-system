8ddba62499db8f47b3a0de583cb05cc8f79e8fa0
Khanh Du <luongkhanhdu95@gmail.com>
2025-05-04 09:11:01 +0700

    Update separate pagination page

---
+++ Models/PaginationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LibrarySystem.Models
+{
+    public class PaginationViewModel
+    {
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+        public string? SearchString { get; set; }
+    }
+
+}
