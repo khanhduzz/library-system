beedae7179d1d704d24366b4eae678639f9c319c
Khanh Du <luongkhanhdu95@gmail.com>
2025-06-01 09:37:02 +0700

    update UI for layout and add sublayout

---
+++ Views/Shared/_SubLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibrarySystem.Views.Shared
+{
+    public class _SubLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
